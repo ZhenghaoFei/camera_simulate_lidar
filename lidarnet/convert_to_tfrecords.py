4e70055501d9c147961ed4aee4d001389534a41c
holly <holly.fei@foxmail.com>
2016-11-20 16:54:13 -0800

    fix

---
+++ lidarnet/convert_to_tfrecords.py
@@ -0,0 +1,143 @@
+# Copyright 2015 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+"""Converts MNIST data to TFRecords file format with Example protos."""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import glob
+import numpy as np
+import tensorflow as tf
+from scipy import misc
+
+
+tf.app.flags.DEFINE_string('directory', '../data/data1/',
+                           'Directory to download data files and write the '
+                           'converted result')
+tf.app.flags.DEFINE_integer('validation_size', 100,
+                            'Number of examples to separate from the training '
+                            'data for the validation set.')
+FLAGS = tf.app.flags.FLAGS
+
+
+def _int64_feature(value):
+  return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+def _float_feature(value):
+  return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
+
+def _bytes_feature(value):
+  return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+# only get serial number
+def get_filenumber(mypath):
+    # mypath = './data/data1/'
+    data_lidar = sorted(glob.glob(mypath + '*txt'), key=os.path.basename)
+    print (mypath)
+
+    file_numbers = []
+    for i in range(len(data_lidar)):
+        number = data_lidar[i]
+        number = number.replace(mypath,"")
+        number = number.replace(".txt","")
+        file_numbers.append(number)
+    print ("number of sample: ", len(file_numbers))
+    return file_numbers
+
+def get_filename_list(file_numbers, data_dir):
+  # reconstruce filename for three source
+  left_image_filename_list = []
+  right_image_filename_list = []
+  lidar_filename_list = []
+
+  for i in range(len(file_numbers)):
+    left_image_filename = data_dir +  file_numbers[i] + "left.png"
+    right_image_filename = data_dir +  file_numbers[i] + "right.png"
+    lidar_filename = data_dir + file_numbers[i] + ".txt"
+    left_image_filename_list.append(left_image_filename)
+    right_image_filename_list.append(right_image_filename)
+    lidar_filename_list.append(lidar_filename)
+
+  return [left_image_filename_list, right_image_filename_list, lidar_filename_list]
+
+def image_filename_list_to_nparray(filename_list):
+    image_nparray = []
+    for k in filename_list:
+        image_nparray.append(misc.imread(k))
+    image_nparray = np.asarray(image_nparray)
+#     print(image_nparray.shape)
+    return image_nparray
+
+def lidar_filename_list_to_nparray(filename_list):
+    lidar_nparray = []
+    for k in filename_list:
+        lidar_nparray.append(np.loadtxt(k, delimiter=','))
+    lidar_nparray = np.asarray(lidar_nparray)
+    # print(lidar_nparray.shape)
+    return lidar_nparray
+
+def convert_to(left_images, right_images, lidar_labels, name):
+  num_examples = lidar_labels.shape[0]
+  if left_images.shape[0] != num_examples:
+    raise ValueError("Images size %d does not match label size %d." %
+                     (left_images.shape[0], num_examples))
+
+  rows_left = left_images.shape[1]
+  cols_left = left_images.shape[2]
+  depth_left = left_images.shape[3]
+
+  rows_right = left_images.shape[1]
+  cols_right = left_images.shape[2]
+  depth_right = left_images.shape[3]
+
+  filename = os.path.join(FLAGS.directory, name + '.tfrecords')
+  print('Writing', filename)
+  writer = tf.python_io.TFRecordWriter(filename)
+  for index in range(num_examples):
+    image_raw_left = left_images[index].tostring()
+    image_raw_right = right_images[index].tostring()
+    lidar_raw = lidar_labels[index].tostring()
+    example = tf.train.Example(features=tf.train.Features(feature={
+        'height_left': _int64_feature(rows_left),
+        'width_left': _int64_feature(cols_left),
+        'depth_left': _int64_feature(depth_left),
+        'height_right': _int64_feature(rows_right),
+        'width_right': _int64_feature(cols_right),
+        'depth_right': _int64_feature(depth_right),
+        'image_raw_left': _bytes_feature(image_raw_left),
+        'image_raw_right': _bytes_feature(image_raw_right),
+        'lidar_label': _bytes_feature(lidar_raw)
+        }))
+    writer.write(example.SerializeToString())
+
+
+def main(argv):
+  # Get the data.
+  file_numbers = get_filenumber(FLAGS.directory)
+  left_image_filename_list, right_image_filename_list, lidar_filename_list = get_filename_list(file_numbers, FLAGS.directory)
+
+  # Extract it into numpy arrays.
+  left_images = image_filename_list_to_nparray(left_image_filename_list)
+  right_images = image_filename_list_to_nparray(right_image_filename_list)
+  lidar_labels = lidar_filename_list_to_nparray(lidar_filename_list)
+
+  # Convert to Examples and write the result to TFRecords.
+  convert_to(left_images, right_images, lidar_labels, 'train')
+
+
+if __name__ == '__main__':
+  tf.app.run()
---
+++ lidarnet/simladar_input.py
@@ -0,0 +1,153 @@
+# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+import glob
+import os
+
+from six.moves import xrange  # pylint: disable=redefined-builtin
+import tensorflow as tf
+
+# Global constants describing the  data set.
+NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN = 1
+NUM_EXAMPLES_PER_EPOCH_FOR_EVAL = 1
+
+IMAGE_HEIGHT = 16
+IMAGE_WIDTH = 64
+
+ORIGINAL_IMAGE_HEIGHT = 128
+ORIGINAL_IMAGE_WIDTH = 672
+IMAGE_DEPTH = 3
+LIDAR_DIM =120
+
+# Constants used for dealing with the files, matches convert_to_records.
+TRAIN_FILE = 'train.tfrecords'
+VALIDATION_FILE = 'validation.tfrecords'
+
+
+def read_and_decode(filename_queue):
+  reader = tf.TFRecordReader()
+  _, serialized_example = reader.read(filename_queue)
+  features = tf.parse_single_example(
+      serialized_example,
+      # Defaults are not specified since both keys are required.
+      features={
+          'image_raw_left': tf.FixedLenFeature([], tf.string),
+          'image_raw_right': tf.FixedLenFeature([], tf.string),
+          'lidar_label': tf.FixedLenFeature([], tf.string),
+      })
+
+  # Convert from a scalar string tensor (whose single string has
+  # length mnist.IMAGE_PIXELS) to a uint8 tensor with shape
+  # [mnist.IMAGE_PIXELS].
+  image_left = tf.decode_raw(features['image_raw_left'], tf.uint8)
+  image_right = tf.decode_raw(features['image_raw_right'], tf.uint8)
+  lidar = tf.decode_raw(features['lidar_label'], tf.float64)
+  print(lidar)
+
+  image_left = tf.reshape(image_left, [ORIGINAL_IMAGE_HEIGHT, ORIGINAL_IMAGE_WIDTH, IMAGE_DEPTH])
+  image_right = tf.reshape(image_right, [ORIGINAL_IMAGE_HEIGHT, ORIGINAL_IMAGE_WIDTH, IMAGE_DEPTH])
+  lidar = tf.reshape(lidar, [LIDAR_DIM])
+  # Convert from uint8 to float32
+  image_left = tf.cast(image_left, tf.float32)
+  image_right = tf.cast(image_right, tf.float32)
+  lidar = tf.cast(lidar, tf.float32)
+
+  # # Image processing for evaluation.
+  # # Crop the central [height, width] of the image.
+  resized_image_left = tf.image.resize_images(image_left, [IMAGE_HEIGHT, IMAGE_WIDTH])
+  resized_image_right = tf.image.resize_images(image_right, [IMAGE_HEIGHT, IMAGE_WIDTH])
+  print(lidar)
+  # # Convert label from a scalar uint8 tensor to an int32 scalar.
+  # label = tf.cast(features['lidar_label'], tf.int32)
+
+  return resized_image_left, resized_image_right, lidar
+
+
+def _generate_image_and_label_batch(left_image, right_image, lidar, min_queue_examples, batch_size, shuffle):
+  """Construct a queued batch of images and labels.
+
+  Args:
+    left_image: 3-D Tensor of [height, width, 3] of type.float32.
+    right_image: 3-D Tensor of [height, width, 3] of type.float32.
+    lidar: 1-D Tensor of [lidar_size] type.float32
+    min_queue_examples: int32, minimum number of samples to retain
+      in the queue that provides of batches of examples.
+    batch_size: Number of images per batch.
+    shuffle: boolean indicating whether to use a shuffling queue.
+
+  Returns:
+    left_image_batch: Images. 4D tensor of [batch_size, height, width, 3] size.
+    right_image_batch: Images. 4D tensor of [batch_size, height, width, 3] size.
+    lidar: Labels. 2D tensor of [batch_size, lidar_size] size.
+  """
+  # Create a queue that shuffles the examples
+  # min_queue_examples = 100
+  num_preprocess_threads = 8
+  if shuffle:
+    left_batch, right_batch, lidar_batch = tf.train.shuffle_batch(
+        [left_image, right_image, lidar],
+        batch_size=batch_size,
+        num_threads=num_preprocess_threads,
+        capacity=min_queue_examples + 3 * batch_size,
+        min_after_dequeue=min_queue_examples)
+  else:
+    left_batch, right_batch, lidar_batch = tf.train.batch(
+        [left_image, right_image, lidar],
+        batch_size=batch_size,
+        num_threads=num_preprocess_threads,
+        capacity=min_queue_examples + 3 * batch_size)
+
+  # Display the training images in the visualizer.
+  tf.image_summary('left_images', left_batch)
+  tf.image_summary('right_images', right_batch)
+
+  return left_batch, right_batch, lidar_batch
+
+
+def inputs(eval_data, data_dir, batch_size):
+  """Construct input for  evaluation using the Reader ops.
+
+  Args:
+    eval_data: bool, indicating if one should use the train or eval data set.
+    data_dir: Path to the data directory.
+    batch_size: Number of images per batch.
+
+  Returns:
+    images: Images. 4D tensor of [batch_size, IMAGE_SIZE, IMAGE_SIZE, 3] size.
+    labels: Labels. 1D tensor of [batch_size] size.
+  """
+  if not eval_data:
+    filename = os.path.join(data_dir, TRAIN_FILE)
+    num_examples_per_epoch = NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN
+  else:
+    filename = os.path.join(data_dir, VALIDATION_FILE)
+    num_examples_per_epoch = NUM_EXAMPLES_PER_EPOCH_FOR_EVAL
+
+  with tf.name_scope('input'):
+    filename_queue = tf.train.string_input_producer([filename], num_epochs=None)
+    resized_image_left, resized_image_right, lidar = read_and_decode(filename_queue)
+
+    # Ensure that the random shuffling has good mixing properties.
+    min_fraction_of_examples_in_queue = 0.4
+    min_queue_examples = int(num_examples_per_epoch *
+                             min_fraction_of_examples_in_queue)
+
+    # Generate a batch of left_images right_images and labels by building up a queue of examples.
+    return _generate_image_and_label_batch(resized_image_left, resized_image_right, lidar, 
+      min_queue_examples, batch_size, shuffle=False)
